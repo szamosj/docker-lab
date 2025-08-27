7ed5854787d6b94420289354d9831d63dc1b4c4b
Bill Newman <william.newman@seattlecolleges.edu>
2025-08-27 01:06:33 +0000

    removes scripts

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 // Database connection
-$servername = "localhost";
+$servername = "db";
 $username = "labuser";
 $password = "labpass";
 $dbname = "testdb";
